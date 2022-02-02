a26136c522622a1208af4d0e56d896f16fa3e929
christianyurianja <chris37fx@gmail.com>
2022-02-02 11:42:25 +0700

    Change from Procedural to OOP

--- latestearthquake/__init__.py
+++ latestearthquake/__init__.py
@@ -1,81 +1,87 @@
 import requests
 from bs4 import BeautifulSoup
 
-description = 'To get the latest earthquake in Indonesia from bmkg.go.id'
+class GempaTerkini:
+    def __init__(self):
+        self.description = 'To get the latest earthquake in Indonesia from bmkg.go.id'
+        self.result = None
+    # Extract data from Website
+    def data_extraction(self):
+        try:
+            content = requests.get('https://bmkg.go.id')
+        except Exception:
+            return None
 
+        if content.status_code == 200:
+            # Get and assign Date and Time data
+            soup = BeautifulSoup(content.text, 'html.parser') #1. INSTATIATION = INSTANSIASI = PENCIPTAAN OBJEK DARI CLASS
+            result = soup.find('span', {'class': 'waktu'})
+            result = result.text.split(', ')
+            date = result[0]
+            time = result[1]
 
-# Extract data from Website
+            # Get and assign magnitude, depth, ls, bt, location, and perceived data
+            result = soup.find('div', {'class', 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+            result = result.findChildren('li')
 
-def data_extraction():
-    try:
-        content = requests.get('https://bmkg.go.id')
-    except Exception:
-        return None
+            i = 0
+            magnitude = None
+            depth = None
+            ls = None
+            bt = None
+            location = None
+            perceived = None
 
-    if content.status_code == 200:
-        # Get and assign Date and Time data
-        soup = BeautifulSoup(content.text, 'html.parser')
-        result = soup.find('span', {'class': 'waktu'})
-        result = result.text.split(', ')
-        date = result[0]
-        time = result[1]
+            for res in result:
+                if i == 1:
+                    magnitude = res.text
+                elif i == 2:
+                    depth = res.text
+                elif i == 3:
+                    coordinate = res.text.split(' - ')
+                    ls = coordinate[0]
+                    bt = coordinate[1]
+                elif i == 4:
+                    location = res.text
+                elif i == 5:
+                    perceived = res.text
+                i = i + 1
 
-        # Get and assign magnitude, depth, ls, bt, location, and perceived data
-        result = soup.find('div', {'class', 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('li')
+            output = dict()
+            output['date'] = date
+            output['time'] = time
+            output['magnitude'] = magnitude
+            output['depth'] = depth
+            output['coordinate'] = {'ls': ls, 'bt': bt}
+            output['location'] = location
+            output['perceived'] = perceived
+            self.result = output
+        else:
+            return None
 
-        i = 0
-        magnitude = None
-        depth = None
-        ls = None
-        bt = None
-        location = None
-        perceived = None
+    # Show the data from extraction
 
-        for res in result:
-            if i == 1:
-                magnitude = res.text
-            elif i == 2:
-                depth = res.text
-            elif i == 3:
-                coordinate = res.text.split(' - ')
-                ls = coordinate[0]
-                bt = coordinate[1]
-            elif i == 4:
-                location = res.text
-            elif i == 5:
-                perceived = res.text
-            i = i + 1
+    def show_data(self):
+        if self.result is None:
+            print('Latest earthquake data is not found')
+            return
+        print('Latest earthquake based on BMKG')
+        print(f"Date: {self.result['date']}")
+        print(f"Time: {self.result['time']}")
+        print(f"Magnitude: {self.result['magnitude']}")
+        print(f"Depth: {self.result['depth']}")
+        print(f"Coordinate: LS={self.result['coordinate']['ls']}, BT={self. result['coordinate']['bt']}")
+        print(f"Location: {self.result['location']}")
+        print(f"Perceived: {self.result['perceived']}")
 
-        output = dict()
-        output['date'] = date
-        output['time'] = time
-        output['magnitude'] = magnitude
-        output['depth'] = depth
-        output['coordinate'] = {'ls': ls, 'bt': bt}
-        output['location'] = location
-        output['perceived'] = perceived
-        return output
-    else:
-        return None
-
-# Show the data from extraction
-
-def show_data(result):
-    if result is None:
-        print('Latest earthquake data is not found')
-        return
-    print('Latest earthquake based on BMKG')
-    print(f"Date: {result['date']}")
-    print(f"Time: {result['time']}")
-    print(f"Magnitude: {result['magnitude']}")
-    print(f"Depth: {result['depth']}")
-    print(f"Coordinate: LS={result['coordinate']['ls']}, BT={result['coordinate']['bt']}")
-    print(f"Location: {result['location']}")
-    print(f"Perceived: {result['perceived']}")
+    def run(self):
+        self.data_extraction()
+        self.show_data()
 
 
 if __name__ == '__main__':
-    result = data_extraction()
-    # show_data(result)
-    print(result)
+    gempa_di_indonesia = GempaTerkini()
+    print('Deskripsi package', gempa_di_indonesia.description)
+    gempa_di_indonesia.run()
+    # gempa_di_indonesia.data_extraction()
+    # gempa_di_indonesia.show_data()
